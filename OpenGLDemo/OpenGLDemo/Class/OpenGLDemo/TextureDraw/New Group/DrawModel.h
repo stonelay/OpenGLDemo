f8fdae74599d7987e74186db13c560f0b7f57553
stonelay <zhanglei19910607@163.com>
2018-07-06 18:10:48 +0800

    可 选择画笔颜色和宽度

---
+++ OpenGLDemo/OpenGLDemo/Class/OpenGLDemo/TextureDraw/New Group/DrawModel.h
@@ -0,0 +1,41 @@
+//
+//  DrawModel.h
+//  OpenGLDemo
+//
+//  Created by LayZhang on 2018/7/6.
+//  Copyright © 2018年 Zhanglei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class DrawElement;
+
+@interface DrawModel : NSObject
+
+@property (nonatomic, strong) DrawElement *drawElement;
+
+@property (nonatomic, strong) NSArray *lineArray;
+
+@end
+
+@interface LineModel : NSObject
+
++ (instancetype)lineWithBeginPoint:(CGPoint)beginPoint
+                          endPoint:(CGPoint)endPoint;
+
+@property (nonatomic, assign) CGPoint beginPoint;
+@property (nonatomic, assign) CGPoint endPoint;
+
+@end
+
+@interface DrawElement : NSObject
+
+@property (nonatomic, assign) CGFloat pointSize;
+
+@property (nonatomic, assign) GLfloat r;
+@property (nonatomic, assign) GLfloat g;
+@property (nonatomic, assign) GLfloat b;
+
+@end
+
+
